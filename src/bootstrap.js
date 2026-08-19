e4d677c4f8af8934a076b92dd55f514af3ddc333
Pedroeusebio1 <154755501+Pedroeusebio1@users.noreply.github.com>
2026-08-19 07:08:18 -0400

    Load card-aware notification sync

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,4 +1,7 @@
 import { initializeCloud } from './cloud.js';
+import { installCardNotificationEnhancer, startCardNoticeSync } from './notifications.js';
 
 await initializeCloud();
+installCardNotificationEnhancer();
 await import('./simple.js');
+startCardNoticeSync();
