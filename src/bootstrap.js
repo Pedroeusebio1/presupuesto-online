aea0f0acabbf0f527e35a42ee9ba625d2f9f9641
Pedroeusebio1 <154755501+Pedroeusebio1@users.noreply.github.com>
2026-08-19 20:26:32 -0400

    Restore app startup after payment UI regression

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,9 +1,7 @@
 import { initializeCloud } from './cloud.js';
 import { installCardNotificationEnhancer, startCardNoticeSync } from './notifications.js';
-import { installPaymentUiEnhancer } from './payment-ui.js';
 
 await initializeCloud();
 installCardNotificationEnhancer();
-installPaymentUiEnhancer();
 await import('./simple.js');
 startCardNoticeSync();
