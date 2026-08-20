7f2fcf4407a09419e2f0d83d921312bd79e587f9
Pedroeusebio1 <154755501+Pedroeusebio1@users.noreply.github.com>
2026-08-19 20:03:37 -0400

    Use correct payment source and destination labels

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,7 +1,9 @@
 import { initializeCloud } from './cloud.js';
 import { installCardNotificationEnhancer, startCardNoticeSync } from './notifications.js';
+import { installPaymentUiEnhancer } from './payment-ui.js';
 
 await initializeCloud();
 installCardNotificationEnhancer();
+installPaymentUiEnhancer();
 await import('./simple.js');
 startCardNoticeSync();
